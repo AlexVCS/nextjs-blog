89bf50f2df7e0e61e8ebe1f58da810cb27a189b6
Alex Curtis-Slep <alexcurtisslep@gmail.com>
2020-11-11 21:38:17 -0500

    Implementing getStaticProps which includes blog data

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,18 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
+import { getSortedPostsData } from '../lib/posts'
 import utilStyles from '../styles/utils.module.css'
 
-export default function Home() {
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
+}
+
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
@@ -10,6 +20,20 @@ export default function Home() {
       </Head>
       <section className={utilStyles.headingMd}>
         <p>Hey I'm Alex. Full stack developer, lifelong learner and vegan. Follow me on{' '}<a href="https://twitter.com/alexcurtisslep">Twitter</a>.</p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   )
