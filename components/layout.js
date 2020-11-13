abe4f7017a286bdbdce7bc880ef5a130b8d765d4
Alex Curtis-Slep <alexcurtisslep@gmail.com>
2020-11-12 21:16:28 -0500

    Continued up to implement getStaticProps

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
 const name = 'Alex Curtis-Slep'
-export const siteTitle = 'Next.js Sample Website'
+export const siteTitle = 'Alex Curtis-Slep'
 
 export default function Layout({ children, home }) {
   return (
